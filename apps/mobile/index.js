ebfed4308a563ea418ca382a2120e0c98bf8c568
Kimpopi <heishia@naver.com>
2026-03-26 00:36:56 +0900

    빌드완료

--- apps/mobile/index.js
+++ apps/mobile/index.js
@@ -1,1 +1,16 @@
+if (typeof globalThis.WeakRef === 'undefined') {
+  globalThis.WeakRef = class WeakRef {
+    constructor(target) { this._target = target; }
+    deref() { return this._target; }
+  };
+}
+
+if (typeof globalThis.FinalizationRegistry === 'undefined') {
+  globalThis.FinalizationRegistry = class FinalizationRegistry {
+    constructor() {}
+    register() {}
+    unregister() {}
+  };
+}
+
 import 'expo-router/entry';
